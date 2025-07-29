3ba6c76d5f12fb41b8c311053c6399f3d39904a3
Shrikant Kale <58430952+kaleshrikant@users.noreply.github.com>
2025-07-29 12:29:55 +0530

    testing : slot allocations

--- smart-parking-system/src/test/java/com/kaleshrikant/SmartParkingSystemApplicationTests.java
+++ smart-parking-system/src/test/java/com/kaleshrikant/SmartParkingSystemApplicationTests.java
@@ -2,12 +2,18 @@ package com.kaleshrikant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.ApplicationModule;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class SmartParkingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ApplicationModules module =  ApplicationModules
+												.of(SmartParkingSystemApplication.class).verify();
+		new Documenter((ApplicationModules) module).writeDocumentation();
 	}
 
 }
